ee3dc35fb9d1b6019349fa120ac56fa33d38d745
sn3h1l <snehilsingh2811@gmail.com>
2025-10-01 21:12:00 +0530

    added streaming options in a different streamlit file

---
+++ streaming_chatbot.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from chatbot_backend import workflow
+from langchain_core.messages import SystemMessage, HumanMessage,BaseMessage
+
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
+
+for messages in st.session_state["message_history"]:
+    with st.chat_message(messages["role"]):
+        st.text(messages["content"])
+
+
+CONFIG={"configurable":{"thread_id":"thread_1"}}
+
+user_input = st.chat_input("Type your message here")
+
+if user_input:
+    st.session_state["message_history"].append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+    node_state = {"messages": [HumanMessage(content=user_input)]}
+    with st.chat_message("assistant"):
+        workflow_response = st.write_stream(message.content for message,metadata in workflow.stream(node_state,config=CONFIG,stream_mode="messages"))
+    st.session_state["message_history"].append({"role": "assistant", "content": workflow_response})
+
+# the st.write stream component streams the response in the UI part and the workflow.stream method streams the response from the flash API in two parts message and metadata
