6026eee5c41ada2e51242b929033d48651ac84c6
sn3h1l <snehilsingh2811@gmail.com>
2025-10-05 21:27:13 +0530

    langsmith integration for threadwise observability for each trace

---
+++ langsmith_threadwise_bot.py
@@ -0,0 +1,114 @@
+import streamlit as st
+from chatbot_backend_database import chatbot
+from langchain_core.messages import SystemMessage, HumanMessage,BaseMessage,AIMessage
+import uuid
+from chatbot_backend_database import retrieve_threads
+import os
+
+os.environ["LANGCHAIN_PROJECT"] = "THREADED_SQLITE_CHATBOT"
+#*************** UTILILTY FUNCTIONS *************************
+
+
+def generate_thread_id():
+    thread_id = uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history'] = []
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+def load_conversation(thread_id):
+    state = chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
+    # Check if messages key exists in state values, return empty list if not
+    return state.values.get('messages', [])
+
+def check_if_messages(thread_id):
+    state = chatbot.get_state(config={'configurable': {'thread_id': thread_id}})
+    if state.values.get('messages'):
+        return state.values.get('messages')[0].content[0:10]
+    else: return str(thread_id)
+
+#***************** SESSION SETUP *************************
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = retrieve_threads()
+
+add_thread(st.session_state['thread_id'])
+
+
+
+#************************SIDEBAR UI**************************
+
+st.sidebar.title('Threaded Chatbot')
+
+if st.sidebar.button('Start a New Chat'):
+    reset_chat()
+
+st.sidebar.header('My Conversations')
+
+for thread_id in st.session_state['chat_threads'][::-1]:
+    if st.sidebar.button(check_if_messages(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = load_conversation(thread_id)
+
+        temp_messages = []
+
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role='user'
+            else:
+                role='assistant'
+            temp_messages.append({'role': role, 'content': msg.content})
+
+        st.session_state['message_history'] = temp_messages
+
+#************************MAIN UI PART*************************
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    CONFIG = {
+            'configurable': {'thread_id': st.session_state['thread_id']},
+            'metadata':{'thread_id': str(st.session_state['thread_id'])},
+            'run_name':'chat_turn',
+              }
+
+     # first add the message to message_history
+    with st.chat_message("assistant"):
+        def ai_only_stream():
+            for message_chunk, metadata in chatbot.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode="messages"
+            ):
+                if isinstance(message_chunk, AIMessage):
+                    # yield only assistant tokens
+                    yield message_chunk.content
+
+        ai_message = st.write_stream(ai_only_stream())
+
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
